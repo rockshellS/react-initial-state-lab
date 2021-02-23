0a357ac8bf8a1ad7aaf96990128ad418c08e22ba
rockshellS <spicer.rachelle@gmail.com>
2021-02-22 20:03:31 -0600

    commit tree 86001c66c5179d804fea89948eacf44fd9451d54

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,24 @@
-// your ImageSlider code here!
+import React from "react"
+
+
+class ImagineSlider extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            currentSlideIndex: 0
+        }
+    }
+
+    
+
+    render() {
+        return (
+             <h1>I am on slide {this.state.currentSlideIndex}</h1>
+        )
+            
+            
+    }
+}
+
+
+export default ImagineSlider
